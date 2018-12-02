e2502c5db77868621d0c534c9012be2167b63330
Evan Anger <devandanger@gmail.com>
2018-12-02 08:58:51 -0500

    Initial revision

---
+++ MVx/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  MVx
+//
+//  Created by Evan Anger on 12/2/18.
+//  Copyright © 2018 Mighty Strong Software. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MVxUITests/Pods-MVxUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVxUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MVxUITests
+@end
